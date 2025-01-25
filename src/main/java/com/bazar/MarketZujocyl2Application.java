e7cc0027ab94b8f54dc3af9ad5e04d8ba9de0022
JosueNanez <josue.esforzador@gmail.com>
2025-01-25 17:40:45 -0500

    DeshaciendoCambios

--- src/main/java/com/bazar/MarketZujocyl2Application.java
+++ src/main/java/com/bazar/MarketZujocyl2Application.java
@@ -2,10 +2,8 @@ package com.bazar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // Tarea programada para solicitudes periódicas
 public class MarketZujocyl2Application {
 
 	public static void main(String[] args) {
